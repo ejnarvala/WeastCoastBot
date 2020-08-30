9f105bdfba1c2e49ec2fee6415bf0109a483d5ae
Ejnar Arechavala <ejnar123@gmail.com>
2020-08-30 17:27:12 -0400

    real commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+const re = /\w+ is over/
+
+client.on("message", msg => {
+    if (msg.author.bot) return
+    if (re.test(msg.content)) {
+        startIdx = msg.content.search(re)
+        endIdx = msg.content.search(' is over')
+        let thing = msg.content.substring(startIdx, endIdx)
+        let reply = '[Fake News Warning]: ' + thing + ' may not actually be over'
+        msg.reply(reply)
+    }
+})
+
+client.login(process.env.BOT_TOKEN)
